ffb5f683963fce6fa303e32ea3bd4d44a155de4e
WizardMeow <WizardMeow@users.noreply.github.com>
2026-08-06 14:14:00 +0800

    remove broken zsh autosuggestions

--- home/dot_zsh/exports.zsh
+++ home/dot_zsh/exports.zsh
@@ -12,9 +12,6 @@ export VISUAL=$HOME/.bin/editor
 export LANG=zh_CN.UTF-8
 export LANGUAGE=zh_CN:en_US
 
-export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
-export ZSH_AUTOSUGGEST_USE_ASYNC=1
-export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 export ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/ohmyzsh"
 mkdir -p "$ZSH_CACHE_DIR/completions"
 
